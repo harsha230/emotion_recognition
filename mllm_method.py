9a6813704875495cfc3308505ce662506ce7cec2
fernandabufon <fernandab.farber@gamil.com>
2025-11-26 16:59:44 -0500

    update

--- mllm_method.py
+++ mllm_method.py
@@ -42,7 +42,7 @@ print(f"Loading dataset...")
 df = pd.read_csv(f"dataset_path_labels.csv")
 
 
-df = df.sample(10).reset_index(drop=True)
+df = df.sample(1).reset_index(drop=True)
 
 print(f"Running {model} on dataset with {method}...")
 for index, row in tqdm(df.iterrows(), total=len(df)):
@@ -50,13 +50,13 @@ for index, row in tqdm(df.iterrows(), total=len(df)):
     print(f"\nExpected Emotion: {row['emotion']}")
     print(f"\nModel: {model} | Method: {method}")
     print(f"\nPrompt: {prompt}\n")
-    result = run_ollama_model(image_path=row["image_path"], prompt=prompt, model=model)
-    df.loc[index, "label"] = row["emotion"]
-    df.loc[index, "image_path"] = row["image_path"]
-    df.loc[index, "model_name"] = result.get("model_name", "N/A")
-    df.loc[index, "response_time"] = result.get("response_time", "N/A")
-    df.loc[index, "prediction_continuos"] = result.get("explanation", "N/A")
-    df.loc[index, "predicted_emotion"] = result.get("final_answer", "N/A")
+    # result = run_ollama_model(image_path=row["image_path"], prompt=prompt, model=model)
+    # df.loc[index, "label"] = row["emotion"]
+    # df.loc[index, "image_path"] = row["image_path"]
+    # df.loc[index, "model_name"] = result.get("model_name", "N/A")
+    # df.loc[index, "response_time"] = result.get("response_time", "N/A")
+    # df.loc[index, "prediction_continuos"] = result.get("explanation", "N/A")
+    # df.loc[index, "predicted_emotion"] = result.get("final_answer", "N/A")
 
 df.to_csv(f"{output_path}.csv", index=False)
 
