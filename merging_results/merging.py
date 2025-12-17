bf5173b0c59d67436a0005fa7bcd55fc5d85fc80
fernandabufon <fernandab.farber@gamil.com>
2025-12-17 13:44:10 -0500

    update

--- merging_results/merging.py
+++ merging_results/merging.py
@@ -19,12 +19,17 @@ names = [
     "qwen3-vl_2b-instruct_discrete",
     "qwen3-vl_32b-instruct_discrete",
     "llava-llama3_8b_discrete",
+    "gemma3_12b-it-q4_K_M_discrete",
+    "gemma3_4b-it-q4_K_M_discrete",
+    "ministral-3_3b-instruct-2512-q4_K_M_discrete",
     "qwen3-vl_8b-instruct_continuous",
     "llava-phi3_3.8b_continuous",
     "llava_7b_continuous",
     "minicpm-v_8b_continuous",
     "qwen3-vl_2b-instruct_continuous",
     "qwen3-vl_4b-instruct_continuous",
+    "gemma3_12b-it-q4_K_M_continuous",
+    "gemma3_4b-it-q4_K_M_continuous",
     "llava_13b_hierarchical",
     "llava_13b_hierarchical",
     "qwen3-vl_8b-instruct_hierarchical",
@@ -32,7 +37,10 @@ names = [
     "llava-llama3_8b_hierarchical",
     "llava-phi3_3.8b_hierarchical",
     "llava_7b_hierarchical",
-    "qwen3-vl_4b-instruct_hierarchical"
+    "qwen3-vl_4b-instruct_hierarchical",
+    "gemma3_12b-it-q4_K_M_hierarchical",
+    "gemma3_4b-it-q4_K_M_hierarchical"
+    
 ]
 
 datasets_list = []
---
+++ merging_results/results2.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+NAMES=(
+  # "qwen3-vl_8b-instruct_continuous"
+  # "llava-phi3_3.8b_continuous"
+  # "llava_7b_continuous"
+  # "minicpm-v_8b_continuous"
+  # "qwen3-vl_2b-instruct_continuous"
+  # "qwen3-vl_4b-instruct_continuous"
+  "gemma3_12b-it-q4_K_M_continuous"
+  "gemma3_4b-it-q4_K_M_continuous"
+)
+
+
+for NAME in "${NAMES[@]}"; do
+  python results_continuous.py \
+    --name "${NAME}"
+done
+
+
+echo "All processes completed."
